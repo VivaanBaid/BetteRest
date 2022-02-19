0c31b2717a396824af5bf08e43b69b91cad6010e
Vivaan Baid <baidvivaan782@gmail.com>
2022-02-19 18:28:14 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vivaan Baid on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
